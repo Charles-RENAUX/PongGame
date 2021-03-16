26e691b9b5f131e469f2c7685b58842d7673c34b
Charles RENAUX <charles.renaux@student.yncrea.fr>
2021-03-16 16:44:42 +0100

    v3: tout modes OK + IA + fonction angles

--- PongRaspb.py
+++ PongRaspb.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 15 11:53:39 2021
+Created on Tue Mar 16 15:02:13 2021
 
 @author: Lenovo
 """
@@ -56,8 +56,61 @@ def game_2players():
     score=canvas.create_text(larg_fenetre/2,50,fill="white",font="Times 40 bold",text=str(scorej1)+" - "+str(scorej2))
     
     
-    
-    
+    def anglej1():
+       global PosXball, PosYball, dx ,dy, PosYj1
+       #detection frappe de la balle entre 5zones de la raquette
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1] and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[1]+20 :
+           #modif angle
+           dx=dx+5
+           dy=dy-6
+           #print("1")
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1]+20 and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[1]+40 :
+           #modif angle
+           dy=dy-4
+           #print("2")
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1]+40 and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[1]+60 :
+           #modif vitesse
+           #print("3")
+           if dx>=10:
+               dx=dx-5
+               #print("-actif")
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1]+60 and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[1]+80 :
+           #modif angle
+           dy=dy+4
+           #print("4")
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1]+80 and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[3] :
+           #modif angle
+           dx=dx+5
+           dy=dy+6
+           #print("5")
+        
+    def anglej2():
+       global PosXball, PosYball, dx ,dy, PosYj2
+       #detection frappe de la balle entre 5zones de la raquette
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ2)[1] and canvas.coords(balle)[1] < canvas.coords(raquetteJ2)[1]+20 :
+           #modif angle
+           dx=dx-5
+           dy=dy-6
+           #print("1")
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ2)[1]+20 and canvas.coords(balle)[1] < canvas.coords(raquetteJ2)[1]+40 :
+           #modif angle
+           dy=dy-4
+           #print("2")
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ2)[1]+40 and canvas.coords(balle)[1] < canvas.coords(raquetteJ2)[1]+60 :
+           #modif vitesse
+           #print("3")
+           if dx>=-10:
+               dx=dx+5
+               #print("-actif")
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ2)[1]+60 and canvas.coords(balle)[1] < canvas.coords(raquetteJ2)[1]+80 :
+           #modif angle
+           dy=dy+4
+           #print("4")
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ2)[1]+80 and canvas.coords(balle)[1] < canvas.coords(raquetteJ2)[3] :
+           #modif angle
+           dx=dx-5
+           dy=dy+6
+           #print("5")
      
     #mapping clavier pour le mouvement des raquettes
     def mouvement(event):
@@ -98,15 +151,19 @@ def game_2players():
         if PosYball < 0 or PosYball > 465:
             dy=-dy
             
-        if canvas.coords(balle)[0] == 480 and canvas.coords(raquetteJ2)[1] <= canvas.coords(balle)[1] <= canvas.coords(raquetteJ2)[3]:#ici la balle ne rebondit pas sur la raquette
+        if canvas.coords(balle)[0] >= 480 and canvas.coords(raquetteJ2)[1] <= canvas.coords(balle)[1] <= canvas.coords(raquetteJ2)[3]:#ici la balle ne rebondit pas sur la raquette
             dx = -dx
-        if canvas.coords(balle)[0] == 20 and canvas.coords(raquetteJ1)[1] <= canvas.coords(balle)[1] <= canvas.coords(raquetteJ1)[3]:#ici la balle ne rebondit pas sur la raquette
+            anglej2()
+        if canvas.coords(balle)[0] <= 20 and canvas.coords(raquetteJ1)[1] <= canvas.coords(balle)[1] <= canvas.coords(raquetteJ1)[3]:#ici la balle ne rebondit pas sur la raquette
             dx = -dx
+            anglej1()
             
         if PosXball<0 :
             #reset position
             PosXball=haut_fenetre/2
             PosYball=larg_fenetre/2
+            dx=-5
+            dy=7
             canvas.coords(balle,PosXball,PosYball,PosXball+10,PosYball+10)
             
             #MAJ du score
@@ -117,6 +174,8 @@ def game_2players():
             #reset position
             PosXball=haut_fenetre/2
             PosYball=larg_fenetre/2
+            dx=5
+            dy=7
             canvas.coords(balle,PosXball,PosYball,PosXball+10,PosYball+10)
             
             #MAJ du score
@@ -165,29 +224,6 @@ def game_2players():
 def game_vsIA():
     global menu
    
-    menu.destroy()#Detruire le menu pour utiliser le score
-    Pong = tk.Tk()
-    Pong.title("[New Game]")
- 
-    canvas = tk.Canvas(Pong,width = 500, height = 500 , bd=0, bg="black")
-    canvas.pack(padx=10,pady=10)
-
-    canvas.focus_set()
-   
-    Pong.mainloop()
-   
-  
-# -------------------------------------------------------------------------------------------------------------
-
-    
-def game_vsWall():
-    PosXball=haut_fenetre/2
-    PosYball=larg_fenetre/2
-        #position raquette 1 et 2
-    PosXj1=10
-    PosYj1=haut_fenetre/2
-
-    
     #init de la fennetre de jeux.
     Pong = tk.Tk()
     Pong.tittle=("Jeu de Pong")
@@ -200,8 +236,57 @@ def game_vsWall():
     #création des elements du jeux
     balle = canvas.create_oval(PosXball,PosYball,PosXball+20,PosYball+20,fill='white') #balle de rayon 20
     raquetteJ1 = canvas.create_rectangle(PosXj1,PosYj1,PosXj1+10,PosYj1+100,fill='blue')
-    score=canvas.create_text(larg_fenetre/2,50,fill="white",font="Times 40 bold",text=str(scorej1))
+    raquetteIA = canvas.create_rectangle(PosXj2,PosYj2,PosXj2+10,PosYj2+100,fill='red')
+    score=canvas.create_text(larg_fenetre/2,50,fill="white",font="Times 40 bold",text=str(scorej1)+" - "+str(scorej2))
     
+    
+    def IA():       
+    
+        #deplacement dans la zone
+       
+        global larg_fenetre
+        if canvas.coords(balle)[2] > larg_fenetre*7/10:
+            if canvas.coords(balle)[3]-10 < canvas.coords(raquetteIA)[1]+50:
+                if canvas.coords(raquetteIA)[1]>=10:
+                    canvas.move(raquetteIA,0,-20)
+                       
+            if canvas.coords(balle)[3]-10 > canvas.coords(raquetteIA)[1]+50:
+                if canvas.coords(raquetteIA)[1]+100<=haut_fenetre:
+                    canvas.move(raquetteIA,0,20)
+                    
+        
+       
+        #print(zoneBall)
+     
+        
+    def angle():
+       global PosXball, PosYball, dx ,dy, PosYj1
+       #detection frappe de la balle entre 5zones de la raquette
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1] and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[1]+20 :
+           #modif angle
+           dx=dx+5
+           dy=dy-6
+           #print("1")
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1]+20 and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[1]+40 :
+           #modif angle
+           dy=dy-4
+           #print("2")
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1]+40 and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[1]+60 :
+           #modif vitesse
+           #print("3")
+           if dx>=10:
+               dx=dx-5
+               #print("-actif")
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1]+60 and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[1]+80 :
+           #modif angle
+           dy=dy+4
+           #print("4")
+       if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1]+80 and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[3] :
+           #modif angle
+           dx=dx+5
+           dy=dy+6
+           #print("5")
+        
      
     #mapping clavier pour le mouvement des raquettes
     def mouvement(event):
@@ -216,6 +301,7 @@ def game_vsWall():
             if canvas.coords(raquetteJ1)[1]+100<=haut_fenetre:
                 canvas.move(raquetteJ1,0,20)
                 #print(canvas.coords(raquetteJ1))
+       
             
     
     
@@ -233,21 +319,169 @@ def game_vsWall():
      
         if PosYball < 0 or PosYball > 465:
             dy=-dy
-            if rdm.randint(1, 10) >7:
-                dy+=rdm.randint(0, 3)
-        if PosXball> larg_fenetre:
-            dx = -dx
-            if rdm.randint(1, 10) >8:
-                dy+=rdm.randint(0, 2)
             
+        if canvas.coords(balle)[0] == 480 and canvas.coords(raquetteIA)[1] <= canvas.coords(balle)[1] <= canvas.coords(raquetteIA)[3]:#ici la balle ne rebondit pas sur la raquette
+            dx = -dx
         if canvas.coords(balle)[0] == 20 and canvas.coords(raquetteJ1)[1] <= canvas.coords(balle)[1] <= canvas.coords(raquetteJ1)[3]:#ici la balle ne rebondit pas sur la raquette
             dx = -dx
+            angle()
+            
+        if PosXball<0 :
+            #reset position
+            PosXball=haut_fenetre/2
+            PosYball=larg_fenetre/2
+            dx=-5
+            dy=7
+            canvas.coords(balle,PosXball,PosYball,PosXball+10,PosYball+10)
+            
+            #MAJ du score
+            scorej2+=1
+            canvas.itemconfig(score,text= str(scorej1)+" - "+str(scorej2))
+            
+        if PosXball > larg_fenetre:
+            #reset position
+            PosXball=haut_fenetre/2
+            PosYball=larg_fenetre/2
+            dx=5
+            dy=7
+            canvas.coords(balle,PosXball,PosYball,PosXball+10,PosYball+10)
+            
             #MAJ du score
             scorej1+=1
-            if scorej1%2==0:
-                dx+=3
-                dy+=2
+            canvas.itemconfig(score,text= str(scorej1)+" - "+str(scorej2))
+            
+            
+        
+        if scorej1>=5 or scorej2>=5:
+            gameOverScreen()
+            
+                
+            
+        canvas.after(20,IA)
+        canvas.after(50,mouvball)
+    
+        
+    def gameOverScreen():
+        #efface les objets
+        canvas.delete(balle)
+        canvas.delete(raquetteJ1)
+        canvas.delete(raquetteIA)
+        canvas.delete(score)
+        #ecran de game over
+        if scorej1>=5:
+            canvas.create_text(larg_fenetre/2,haut_fenetre/2,fill="blue",font="Times 40 bold",text="Victoire J1 : "+ str(scorej1)+" - "+str(scorej2))
+            canvas.after(3000,Pong.destroy)
+            canvas.after(2999,creatMenu)
+        if scorej2>=5:
+            canvas.create_text(larg_fenetre/2,haut_fenetre/2,fill="red",font="Times 40 bold",text="Victoire IA : "+ str(scorej2)+" - "+str(scorej1))    
+            canvas.after(3000,Pong.destroy)
+            canvas.after(2999,creatMenu)
+    
+    
+    canvas.focus_set()#on met le focus sur le canvas
+    Pong.focus_force()
+    #on dit au canvas de prendre en compte les actions de mouvement(event)
+    #on ne doit pas laisser les touches enfonce
+    canvas.bind('<Key>',mouvement) 
+    
+    mouvball()
+    Pong.mainloop()
+   
+  
+# -------------------------------------------------------------------------------------------------------------
+
+    
+def game_vsWall():
+    PosXball=haut_fenetre/2
+    PosYball=larg_fenetre/2
+        #position raquette 1 et 2
+    PosXj1=10
+    PosYj1=haut_fenetre/2
+
+    
+       #init de la fennetre de jeux.
+    Pong = tk.Tk()
+    Pong.tittle=("Jeu de Pong")
+    
+    #Création du canvas + ajout a la fen
+    canvas = tk.Canvas(Pong,width = 500, height = 500 , bd=0, bg="black")
+    canvas.pack(padx=10,pady=10)
+    
+    
+    #création des elements du jeux
+    balle = canvas.create_oval(PosXball,PosYball,PosXball+20,PosYball+20,fill='white') #balle de rayon 20
+    raquetteJ1 = canvas.create_rectangle(PosXj1,PosYj1,PosXj1+10,PosYj1+100,fill='blue')
+    score=canvas.create_text(larg_fenetre/2,50,fill="white",font="Times 40 bold",text=str(scorej1))
+    
+     
+    #mapping clavier pour le mouvement des raquettes
+    def mouvement(event):
+        global PosYj1
+        Key = event.keysym
+     
+        if Key == 'z':
+            if canvas.coords(raquetteJ1)[1]>=10:
+                canvas.move(raquetteJ1,0,-20)
+                #print(canvas.coords(raquetteJ1))
+        if Key == 's':
+            if canvas.coords(raquetteJ1)[1]+100<=haut_fenetre:
+                canvas.move(raquetteJ1,0,20)
+                #print(canvas.coords(raquetteJ1))
+            
+    
+    def angle():
+        global PosXball, PosYball, dx ,dy, PosYj1
+        #detection frappe de la balle entre 5zones de la raquette
+        if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1] and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[1]+20 :
+            #modif angle
+            dx=dx+5
+            dy=dy-6
+            #print("1")
+        if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1]+20 and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[1]+40 :
+            #modif angle
+            dy=dy-4
+            #print("2")
+        if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1]+40 and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[1]+60 :
+            #modif vitesse
+            #print("3")
+            if dx>=10:
+                dx=dx-5
+                #print("-actif")
+        if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1]+60 and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[1]+80 :
+            #modif angle
+            dy=dy+4
+            #print("4")
+        if canvas.coords(balle)[1]>=canvas.coords(raquetteJ1)[1]+80 and canvas.coords(balle)[1] < canvas.coords(raquetteJ1)[3] :
+            #modif angle
+            dx=dx+5
+            dy=dy+6
+            #print("5")
+    
+    def mouvball():#fonction qui fait bouger la balle et qui gere les colision avec les bordures
+        global PosXball, PosYball, dx ,dy,scorej1,scorej2
+     
+        PosXball = PosXball + dx
+     
+        PosYball = PosYball + dy
+        
+        #print("balle:")
+        #print(canvas.coords(balle))
+     
+        canvas.coords(balle,PosXball,PosYball,PosXball+10,PosYball+10)
+     
+        if PosYball < 0 or PosYball > 465:
+            dy=-dy
+            
+        if PosXball> larg_fenetre:
+            dx = -dx
+            
+        if canvas.coords(balle)[0] <= 20 and canvas.coords(raquetteJ1)[1] <= canvas.coords(balle)[1] <= canvas.coords(raquetteJ1)[3]:#ici la balle ne rebondit pas sur la raquette
+            dx = -dx
+            angle()
+            #MAJ du score
+            scorej1+=1
             canvas.itemconfig(score,text= str(scorej1))
+            
             
         if PosXball<0 :
             #reset position
@@ -255,7 +489,7 @@ def game_vsWall():
             PosYball=larg_fenetre/2
             canvas.coords(balle,PosXball,PosYball,PosXball+10,PosYball+10)
             gameOverScreen()
-
+    
         canvas.after(50,mouvball)
     
         
@@ -334,8 +568,10 @@ def creatMenu():
         menu.after(110, menu.destroy)
     
     def onCanvasClick2(event):
-         menu.after(100,game_vsIA)
-         menu.after(110, menu.destroy)
+        global scorej1,scorej2
+        scorej1,scorej2 = 0, 0
+        menu.after(100,game_vsIA)
+        menu.after(110, menu.destroy)
          
     def onCanvasClick3(event):
         global scorej1
